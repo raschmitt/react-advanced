d1dc4e7125f98aa68f7ea90f0bb92258af0e1537
Rogerio Schmitt <rogerio.schmitt@me.com>
2021-04-12 21:28:30 -0300

    Add first component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,14 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" />
+    <S.Tille>React - NextJs Sample</S.Tille>
+    <S.Description>
+      Typescript, ReactJs, NextJs & Styled Components
+    </S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" />
+  </S.Wrapper>
 )
 
 export default Main
